4eea7bdd0fb9ca2b86f1d84e5ac64a34f29cdf13
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-14 21:29:54 +0200

    jQuery JS papildymai, title

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,26 +1,9 @@
-// $(document).ready(function() {
-//     console.log('Hello jQuery');
-// });
-
-// $(function() {
-//     $('.boxes').on('click', function() {
-//         console.log('clicked on a box');
-//     })
-// })
-
-// $(function() {
-//     $('.boxes').on('click', function() {
-//         $(this).addClass('active')
-//     })
-// })
 $(function() {
+
     $('#burger').on('click', function() {
         $('#side-menu').toggleClass('side-menu--closed');
+        $('body').toggleClass('sidenav-closed');
     })
-})
-
-$(function() {
-    $('#burger').on('click', function() {
-        $('#dashboard').toggleClass('section-container--moved');
-    })
+    var basename = $('#side-menu').find('li.active span').text();
+    $('#basename').text(basename).attr('href', '/' + basename.toLowerCase() + 'html');
 })
